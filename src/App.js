835717b55780e57b50ff329ec756e26e949ad185
Fredys Fernandez <juniorfrey2990gmail.com>
2021-12-13 13:49:30 -0500

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App
+
